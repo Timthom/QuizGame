446c31523fa16e79c1a3b252063aa5904f6eca56
Thomas Månsson <thomasmonsson@gmail.com>
2016-02-03 18:24:31 +0100

    Initial Commit

---
+++ Labb2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Labb2
+//
+//  Created by Thomas on 2016-02-03.
+//  Copyright © 2016 Thomas Månsson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Labb2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Labb2
+//
+//  Created by Thomas on 2016-02-03.
+//  Copyright © 2016 Thomas Månsson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
